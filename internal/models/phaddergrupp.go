4378ac82034e7e0c63863e8313a3776f994792d4
Melker Widén <melker.widen@gmail.com>
2025-06-27 20:27:47 +0200

    Add Swish number to phaddergrupp

--- internal/models/phaddergrupp.go
+++ internal/models/phaddergrupp.go
@@ -1,13 +1,15 @@
 package models
 
 type Phaddergrupp struct {
+	SwishNumber string
 	PrimaryColor string
 	SecondaryColor string
 	Icon string
 }
 
-func NewPhaddergrupp(primaryColor string, secondaryColor string, icon string) *Phaddergrupp {
+func NewPhaddergrupp(swishNumber string, primaryColor string, secondaryColor string, icon string) *Phaddergrupp {
 	return &Phaddergrupp{
+		SwishNumber: swishNumber,
 		PrimaryColor: primaryColor,
 		SecondaryColor: secondaryColor,
 		Icon: icon,
