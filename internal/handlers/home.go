92a993fd62fc2a7de537cf930b8946b78f1f8a5d
Melker Widén <melker.widen@gmail.com>
2025-07-19 21:58:13 +0200

    Fix fragile phaddergrupp creation
    Now uses transaction

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -55,16 +55,27 @@ func PostHome(c echo.Context) error {
 	}
 
 	database := db.GetDB(c)
+	userAccountID := auth.GetUserAccountID(c)
 
+	tx, err := database.Begin()
+	if err != nil {
+		c.Logger().Errorf("Failed to begin transaction during phaddergrupp creation: %v", err)
+		return unexpectedFormError()	
+	}
+	defer tx.Rollback()
 	phaddergruppID, err := database.CreatePhaddergrupp(database, phaddergruppName)
 	if err != nil {
 		c.Logger().Errorf("Database error during phaddergrupp creation: %v", err)
 		return unexpectedFormError()	
 	}
-	userAccountID := auth.GetUserAccountID(c)
-	err = database.CreatePhaddergruppMapping(database, userAccountID, phaddergruppID, roles.Phadder)
+	err = database.CreatePhaddergruppMapping(tx, userAccountID, phaddergruppID, roles.Phadder)
 	if err != nil {
-		c.Logger().Errorf("Database error during phaddergrupp mapping creation: %v", err)
+		c.Logger().Errorf("Database error during phaddergrupp mapping creation during phaddergrupp creation: %v", err)
+		return unexpectedFormError()	
+	}
+	err = tx.Commit()
+	if err != nil {
+		c.Logger().Errorf("Database error during user creation: %v", err)
 		return unexpectedFormError()	
 	}
 
