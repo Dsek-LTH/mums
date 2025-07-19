9713c633798c64ec7a7843a98336389277e52fba
Melker Widén <melker.widen@gmail.com>
2025-07-19 04:13:49 +0200

    Add placeholder frontend and logic for route `phaddergrupp/:phaddergrupp-id`

--- internal/handlers/phaddergrupp.go
+++ internal/handlers/phaddergrupp.go
@@ -6,8 +6,17 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type phaddergruppPageData struct {
+	IsLoggedIn                bool
+	AllowedErrorCodes         []int
+	UserProfileName           string
+	PhaddergruppName          string
+	Errors                    map[string][]string
+}
+
 func GetPhaddergrupp(c echo.Context) error {
-	return c.Render(http.StatusOK, "phaddergrupp", map[string]interface{}{})
+	pageData := phaddergruppPageData{}
+	return c.Render(http.StatusOK, "phaddergrupp", pageData)
 }
 
 func PostPhaddergrupp(c echo.Context) error {
